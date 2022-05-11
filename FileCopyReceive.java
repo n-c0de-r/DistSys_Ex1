f7d55e36bebf6048ebc6f7161c4098c636287b1e
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2022-05-11 17:19:32 +0200

    Trying to fix the client, not working though...

--- FileCopyReceive.java
+++ FileCopyReceive.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -25,36 +27,52 @@ public final class FileCopyReceive {
 	 * @throws IOException if there's an I/O related problem
 	 */
 	static public void main (final String[] args) throws IOException {
-		final Path sourcePath = Paths.get(args[0]);
-		if (!Files.isReadable(sourcePath)) throw new IllegalArgumentException(sourcePath.toString());
-
+		final Path destinationPath = Paths.get(args[0]);
+		String ip = "127.0.0.1";
+//		int port = 0;
+		int port = 4711; // Test port
+		
+		if (args.length < 3) {
+			if(args[1] != null || args[1] != "") {
+				port = Integer.parseInt(args[1]);
+			}
+		}
+		if (args.length == 3) {
+			if(args[1] != null || args[1] != "") {
+				ip = args[1];
+			}
+			if(args[2] != null || args[2] != "") {
+				port = Integer.parseInt(args[2]);
+			}
+		}
+		
+		final String finIP = ip;
+		final int finPort = port;
+		
 		final Path sinkPath = Paths.get(args[1]);
 		if (sinkPath.getParent() != null && !Files.isDirectory(sinkPath.getParent())) throw new IllegalArgumentException(sinkPath.toString());
 
-	//	Files.copy(sourcePath, sinkPath, StandardCopyOption.REPLACE_EXISTING);
+		// Files.copy(sourcePath, sinkPath, StandardCopyOption.REPLACE_EXISTING);
 		// Client, IP & Port
-		final PipedOutputStream output = new PipedOutputStream();
-		final PipedInputStream input = new PipedInputStream(output);
 		
 		final byte[] buffer = new byte[0x10000];
 		
 		Runnable transporterOutput = () -> {
-			try (OutputStream fos = Files.newOutputStream(sinkPath)){
+			try (OutputStream fos = Files.newOutputStream(sinkPath);
+					Socket client = new Socket(finIP, finPort);){
+				
+				InputStream input = client.getInputStream();
+				
 				while(input.read() != -1) {
 //					fos.write(input.read(buffer));
 					fos.write(buffer, 0, input.read(buffer));
 				}
+				
 				fos.close();
 				input.close();
 				System.out.println("done.");
 			} catch (IOException e) {
 				e.printStackTrace(System.err);
-			} finally {
-				try {
-					input.close();
-				} catch (IOException ex) {
-					ex.printStackTrace(System.err);
-				}
 			}
 		};
 		
